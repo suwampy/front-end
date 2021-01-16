e1281e7e538376599bcf6deccf21e43fd7613900
suwampy <tndhks123@nate.com>
2021-01-16 16:53:08 +0900

    끝말잇기

---
+++ React/2. react-ts/01. 구구단, 끝말잇기/WordRelay.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import {useState,useRef, useCallback} from 'react';
+
+
+const WordRelay =()=> {
+    const [word, setWord] = useState('유수완');
+    const [value, setValue] = useState('');
+    const [result, setResult] = useState('');
+    const inputEl = useRef<HTMLInputElement>(null);
+
+    const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value)
+    }, []);
+
+    const onSubmitForm = useCallback<(e: React.FormEvent) => void>((e) => {
+        e.preventDefault();
+        const input = inputEl.current;
+        if (word[word.length - 1] === value[0]) {
+            setResult('딩동댕');
+            setWord(value);
+            setValue('');
+            if (input) {
+                input.focus();
+            }
+        } else {
+            setResult('땡');
+            setValue('');
+            if (input) {
+                input.focus();
+            }
+        }
+    }, [word, value]); // word와 value가 바뀌면 새로 만들어진다
+
+    return (
+        <>
+            <div>{word}</div>
+            <form onSubmit = {onSubmitForm}>
+                <input
+                    ref = {inputEl}
+                    value ={value}
+                    onChange={onChange}
+                />
+                <button>입력!</button>
+            </form>
+            <div>{result}</div>
+
+        </>
+    )
+};
+
+export default WordRelay;
